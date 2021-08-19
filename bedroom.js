e0c4f6014a5c2dcc4c51bc3b52b1b20ed2adb9b4
MichelleSWebApps <87533323+MichelleSWebApps@users.noreply.github.com>
2021-08-19 13:04:20 +0530

    Add files via upload

--- bedroom.js
+++ bedroom.js
@@ -1,5 +1,6 @@
 img = "";
 status = "";
+objects = "";
 
 function preload()
 {
@@ -28,10 +29,28 @@ function gotResult(error, results)
         console.log(error);
     }
     console.log(results);
-    objects="results";
+    objects= results;
 }
 
 function navigate()
 {
     window.location.href = "index.html"
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    if(status != "")
+    {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " "+percent+ "%", objects[i].x +15, objects[i].y +15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects.height);
+        }
+    }
 }
--- bottle.js
+++ bottle.js
@@ -1,9 +1,10 @@
 img = "";
 status = "";
+objects = "";
 
 function preload()
 {
-    img = loadImage('bottle.jpg');
+    img = loadImage('bottle.png');
 }
 
 function setup()
@@ -28,10 +29,28 @@ function gotResult(error, results)
         console.log(error);
     }
     console.log(results);
-    objects="results";
+    objects= results;
 }
 
 function navigate()
 {
     window.location.href = "index.html"
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    if(status != "")
+    {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " "+percent+ "%", objects[i].x +15, objects[i].y +15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects.height);
+        }
+    }
 }
